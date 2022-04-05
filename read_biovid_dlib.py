e4a4bd4a79731abc375e99afe418fc0c5be7d766
Francesco Gigli <francesco.gigli@stud.unifi.com>
2022-04-05 12:00:05 +0200

    Fix (commit): replace read_biovid function

--- read_biovid_dlib.py
+++ read_biovid_dlib.py
@@ -1,75 +1,31 @@
 import os
-import json
+import numpy as np
 import pandas as pd
 from pathlib import Path 
-from json import JSONEncoder
-import numpy as np
 
-path_landmarks = 'C:/Users/gigli/Downloads/Landmarks-Biovid-Dlib/landmarkall/' # Path to Biovid-landmarks-labels.zip extracted
-subjects = [name for name in sorted(os.listdir(path_landmarks))]  # List of subjects
-
-
-
-# FIRST correspond to first element of dataset used to costruct training or test file 
-# LAST correspond to last element of dataset 
-
-FIRST = 0
-LAST = len(subjects)
-print(subjects)
-print(LAST)
-file_name = 'dataset_final_' + str(LAST-FIRST) + '.csv'
+path = 'data/dataset/prova/' # Path the landmark folder containig the dataset
+subjects = [name for name in os.listdir(path)] # Retrieve all the subjects in the dataset
 
 filepath = Path('BioVid_Dataset/result.csv')  
-filepath.parent.mkdir(parents=True, exist_ok=True)  
 
-path_save = '/path_save/' + file_name # Path where save csv file
-
-
-lst = []
-element = []
-
-for i in range(FIRST,LAST):
-
-    subject = subjects[i]
-    sequences_path = path_landmarks + subject + '/'
-    sequences = [name for name in sorted(os.listdir(sequences_path))]  # List of sequences
-
-
-    for num_seq in range(0,len(sequences)):
-
-        element.append(i)
-        element.append(num_seq)
-
-        print(element)
-
-        seq = sequences[num_seq]
-        frame = sequences_path + seq
-        # Open JSON file and read coordinates
-        with open(frame, 'r') as f:
+for i, sub in enumerate(subjects):
     
-            json_dict = np.load(f)
-            if len(json_dict['people']) != 0:
-                landmarks = json_dict['people'][0]['face_keypoints_2d']
-            else:
-                print(subject , " - " , num_seq)
+    if sub == '102309_m_61': # Subject to ignore
+        continue
+    
+    path_sequences = path + sub + '/'
+    sequences = [name for name in os.listdir(path_sequences)] # Retrieve all the sequences of the current subject
+    
+    for seq in sequences:
+            
+        path_frame = path_sequences + seq + '/'
+        frames = [name for name in os.listdir(path_frame)] # Retrieve all the frames of the current sequence
+        
+        for k, frame in enumerate(frames):
+            data = np.load(path_frame + frame) # Read the data from the *.npy files (size is (68,2))
 
-            # Get x and y coordinates (third component is a confidence score)
-            ll_x = [landmarks[x] for x in range(0, len(landmarks), 3)]
-            ll_y = [landmarks[x] for x in range(1, len(landmarks), 3)]
-            confidence = [landmarks[x] for x in range(2, len(landmarks), 3)]
-
-            for k in ll_x:
-                element.append(k)
-            for k in ll_y:
-                element.append(k)
-
-        lst.append(element)
-        element = []
-
-
-col = range(0,142)
-df = pd.DataFrame(lst,columns=col)
-print(df)
-#df.to_csv(index=False)   
-df.to_csv(filepath, index=False)
-
+     
+            df = pd.DataFrame(data)
+            print("Data: ",df)
+            df.to_csv(filepath, index=False)
+  
